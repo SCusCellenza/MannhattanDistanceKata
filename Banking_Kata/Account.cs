e80eea25b1ccd413e77ae0f66ebbe594e2de1f66
Sven Cusseau <sven.cusseau@cellenza.com>
2024-03-07 11:23:41 +0100

    Raname solution and create new project for banking kata

---
+++ Banking_Kata/Account.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+
+namespace Banking_Kata
+{
+    internal class Account
+    {
+        public int amount;
+        public List<String> statement;
+
+        public Account()
+        {
+            statement = new List<String> {"Date Amount Balance"};
+        }
+
+        public void deposit(int depositAmount)
+        {
+            amount += depositAmount;
+            statement.Add($"{ DateTime.Today} +{depositAmount} {amount}");
+        }
+
+        public void withdraw(int withdramAmount)
+        {
+            amount -= withdramAmount;
+            statement.Add($"{DateTime.Today} -{withdramAmount} {amount}");
+        }
+
+        public String printStatement()
+        {
+            String printedStatement = "";
+            foreach (String statement in statement)
+            {
+                printedStatement += statement + "\n";
+            }
+
+            return printedStatement;
+        }
+    }
+}
---
+++ Banking_Kata/UnitTest1.cs
@@ -0,0 +1,61 @@
+namespace Banking_Kata
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void WhenADepositInMade_BankAccountInceasesByThisAmount()
+        {
+            //Arrange 
+            Account account = new Account();
+
+            //Act
+            account.deposit(500);
+
+            //Assert
+            Assert.Equal(500, account.amount);
+        }
+
+        [Fact]
+        public void WhenAWithdrawIsDone_BankAccountDecreasesByThisAmount()
+        {
+            //Arrange 
+            Account account = new Account();
+
+            //Act
+            account.withdraw(500);
+
+            //Assert
+            Assert.Equal(-500, account.amount);
+        }
+
+        [Fact]
+        public void WhenAPrintStatementIsAskedAfterTheFirstDeposit_DateAmountAndBalanceArePrinted()
+        {
+            //Arrange 
+            Account account = new Account();
+            string expectedOutput = "Date Amount Balance\n" +
+                                    $"{DateTime.Today} +500 500\n";
+            //Act
+            account.deposit(500);
+
+            //Assert
+            Assert.Equal(expectedOutput, account.printStatement());
+        }
+
+        [Fact]
+        public void WhenAPrintStatementIsAskedAfterADepositAndAWithdraw_DateAmoutAndBalanceArePrinted()
+        {
+            //Arrange 
+            Account account = new Account();
+            string expectedOutput = "Date Amount Balance\n" +
+                                    $"{DateTime.Today} +500 500\n" +
+                                    $"{DateTime.Today} -100 400\n";
+            //Act
+            account.deposit(500);
+            account.withdraw(100);
+
+            //Assert
+            Assert.Equal(expectedOutput, account.printStatement());
+        }
+    }
+}
