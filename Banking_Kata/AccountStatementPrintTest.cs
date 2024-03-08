31f1649cd6d9768d0e431c4069d62fa844c10584
Sven Cusseau <sven.cusseau@cellenza.com>
2024-03-08 09:55:49 +0100

    AccountStatementPrint test depend on DTO and not on account class anymore

--- Banking_Kata/AccountStatementPrintTest.cs
+++ Banking_Kata/AccountStatementPrintTest.cs
@@ -7,32 +7,33 @@ namespace Banking_Kata
         public void WhenAPrintStatementIsAskedAfterTheFirstDeposit_DateAmountAndBalanceArePrinted()
         {
             //Arrange 
-            Account account = new Account();
+            List<BankingTransactionDTO> statement = new List<BankingTransactionDTO>();
             string expectedOutput = "Date Amount Balance\n" +
                                     $"{DateTime.Today.ToString("d")} +500 500\n";
             
             //Act
-            account.Deposit(500);
+            statement.Add(new BankingTransactionDTO(DateTime.Today, 500, 500));
+
 
             //Assert
-            Assert.Equal(expectedOutput, AccountStatementPrint.PrintStatement(account.statement));
+            Assert.Equal(expectedOutput, AccountStatementPrint.PrintStatement(statement));
         }
 
         [Fact]
         public void WhenAPrintStatementIsAskedAfterADepositAndAWithdraw_DateAmoutAndBalanceArePrinted()
         {
             //Arrange 
-            Account account = new Account();
+            List<BankingTransactionDTO> statement = new List<BankingTransactionDTO>();
             string expectedOutput = "Date Amount Balance\n" +
                                     $"{DateTime.Today.ToString("d")} +500 500\n" +
                                     $"{DateTime.Today.ToString("d")} -100 400\n";
 
             //Act
-            account.Deposit(500);
-            account.Withdraw(100);
+            statement.Add(new BankingTransactionDTO(DateTime.Today, 500, 500));
+            statement.Add(new BankingTransactionDTO(DateTime.Today, -100, 400));
 
             //Assert
-            Assert.Equal(expectedOutput, AccountStatementPrint.PrintStatement(account.statement));
+            Assert.Equal(expectedOutput, AccountStatementPrint.PrintStatement(statement));
         }
     }
 }
