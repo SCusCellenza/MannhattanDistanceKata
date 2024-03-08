6497dfb937d2dbb7baaa996602b56507ce024175
Sven Cusseau <sven.cusseau@cellenza.com>
2024-03-08 14:41:23 +0100

    Simplify to a single if statement

--- LeapYearsKata/Year.cs
+++ LeapYearsKata/Year.cs
@@ -10,12 +10,9 @@ namespace LeapYearsKata
         }
         public bool IsLeapYear()
         {
-            if (IsYearDivisibleBy(400))
+            if (IsYearDivisibleBy(400) || (IsYearDivisibleBy(4) && !IsYearDivisibleBy(100)))
                 return true;
-            if (IsYearDivisibleBy(100) && !IsYearDivisibleBy(400))
-                return false;
-            if (IsYearDivisibleBy(4) && !IsYearDivisibleBy(100))
-                return true;
+
             return false;
         }
 
